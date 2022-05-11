509f5ca322e97b04cdc10c74aaf1ac9c40df214b
Sebbe <sebastian_hellman@live.se>
2022-05-11 15:36:27 +0100

    rewrote the api to using the axios package, one test written

--- src/CityPop/api/api.test.ts
+++ src/CityPop/api/api.test.ts
@@ -1,8 +1,11 @@
-import { getCountryCode } from './geonames';
+import * as API from './geonames';
 
 
-
-
-test('Get the country code from sweden', () => {
-    expect(getCountryCode("Sweden")).toBe("SE")
-});
+test('Get the country code from sweden', async () => {
+    
+    const cc = await API.getCountryCode("Sweden");
+    
+    expect(cc).toBe("SE");
+    
+    
+}, 5000);
--- src/CityPop/api/geonames.ts
+++ src/CityPop/api/geonames.ts
@@ -1,17 +1,22 @@
 import { API_NINJA_KEY } from './key';
 
+const axios = require('axios');
+
 export async function searchCity(city: string ): Promise<any> {
     // Searches the API for a city
     var url = 'http://api.geonames.org/searchJSON?name=' + city + '&username=weknowit&maxRows=1&featureClass=P';
     
     try {
-        let res = await fetch(url);
-        let data = await res.json();
-        var cityRes = data['geonames'][0]['name'];
+        let data = await axios.get(url); 
+        
+        var cityRes = data['data']['geonames'][0]['name'];
+        console.log(cityRes);
+
+
         
         return cityRes
     } catch (e) {
-        
+        console.log(e);
     }
 }
 
@@ -19,13 +24,14 @@ export async function getCityPopulation(city: string ): Promise<any> {
     // Searches the API for the population of the city
     var url = 'http://api.geonames.org/searchJSON?name=' + city + '&username=weknowit&maxRows=1&featureClass=P';
     try {
-        let res = await fetch(url);
-        let data = await res.json();
-        var popRes = data['geonames'][0]['population'];
+        let data = await axios.get(url);
+        
+        var popRes = data['data']['geonames'][0]['population'];
+        
 
         return popRes;
     } catch (e) {
-        
+        console.log(e);
     }
     
     
@@ -35,9 +41,10 @@ export async function getCitiesInCountry(country: string, countryCode: string): 
     // Searches the API for cites in the country and sorts them by population
     var url = 'http://api.geonames.org/searchJSON?q=' + countryCode + '&username=weknowit&featureClass=P&orderby=population&country=' + countryCode;
     try {
-        let res = await fetch(url);
-        let data = await res.json();
-        var objects = data['geonames'];
+        let data = await axios.get(url);
+        
+        var objects = data['data']['geonames'];
+
         if (objects.length > 0) {
             return objects;
         } else {
@@ -54,12 +61,14 @@ export async function getCountryCode(country: string): Promise<any> {
     // uses the ninja API to get the country code
     var url = 'https://api.api-ninjas.com/v1/country?name=' + country;
     try {
-        let res = await fetch(url, { headers: {'X-Api-Key': API_NINJA_KEY}});
-        let data = await res.json();
-        var countryCode = data[0]['iso2'];
+        
+        let res = await axios.get(url, { headers: {'X-Api-Key': API_NINJA_KEY}});
+        
+        
+        var countryCode = res['data'][0]['iso2'];
     
         return countryCode
     } catch (e) {
-
+        console.log("Error: " + e)
     }
 }
